41002b3e8dbe417b21e79e38793ae76f66f78626
Misklahr <miknil93@gmail.com>
2015-03-31 16:50:52 +0200

    Hello compass

--- app/src/main/java/com/misklahr/hellocompass/MainActivity.java
+++ app/src/main/java/com/misklahr/hellocompass/MainActivity.java
@@ -1,17 +1,35 @@
 package com.misklahr.hellocompass;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private Button compass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        compass = (Button) findViewById(R.id.compassButton);
+
+        compass.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Intent startGame = new Intent("android.intent.action.COMPASSACTIVITY");
+                startActivity(startGame);
+
+            }
+        });
+
     }
 
 
